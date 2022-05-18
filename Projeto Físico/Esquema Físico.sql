40cb4c372ac7cff973a0d9bccb16b460c5a44425
Marcelo Cristian <marcelo.cristian.969@gmail.com>
2022-05-17 22:29:24 -0300

    esquema fisico done

--- Projeto Físico/Esquema Físico.sql
+++ Projeto Físico/Esquema Físico.sql
@@ -1,26 +1,115 @@
 -- tabela ELEITOR
 create table ELEITOR (
-    nome VARCHAR2(100) not null,
-    endereco VARCHAR2(100) not null,
-    tit_eleitor VARCHAR2(12),
-    constraint pk_eleitor primary key (tit_eleitor)
-    -- constraint chk_tit_eleitor check (tit_eleitor like '????????????')
+    nome VARCHAR2(100),
+    tit_eleitor VARCHAR2(12) primary key,
+    rua VARCHAR2(100),
+    cep VARCHAR2(10),
+    numero number
 );
 SELECT * FROM ELEITOR;
 DROP TABLE ELEITOR;
--- tabela ELEITOR
+
+
+--tabela VOTA
+create table VOTA(
+    tit_eleitor varchar(12),
+    cpf varchar2(14),
+    registro varchar(50),
+    constraint pk_vota primary key (tit_eleitor, cpf),
+    constraint fk_cpf_vota foreign key (cpf) references POLITICO(cpf),
+    constraint fk_titeleitor_cria foreign key (tit_eleitor) references ELEITOR(tit_eleitor)
+);
+SELECT * FROM VOTA;
+DROP TABLE VOTA;
+
+
+-- tabela POLITCO
+create table POLITICO (
+    cpf varchar2(14) primary key,
+    cargo varchar2(50) not null,
+    estado varchar2(2) not null,
+    nome varchar2(100) not null,
+    partido varchar2(10)
+);
+SELECT * FROM POLITICO;
+DROP TABLE POLITICO;
+
+
+-- tabela LEGISLATIVO
+create table LEGISLATIVO (
+    cpf varchar2(14) primary key,
+    cpf_pres varchar2(14),
+    tipo_camara varchar2(50),
+    constraint fk_cpf_pres foreign key (cpf_pres) references LEGISLATIVO(cpf),
+    constraint fk_tipo_camara foreign key (tipo_camara) references CAMARA(tipo)
+);
+SELECT * FROM LEGISLATIVO;
+DROP TABLE LEGISLATIVO;
+
+
+-- tabela CAMARA
+create table CAMARA(
+    tipo varchar2(50) primary key,
+    presidente varchar2(100)
+);
+SELECT * FROM CAMARA;
+DROP TABLE CAMARA;
+
+
+-- tabela EXECUTIVO
+create table EXECUTIVO (
+    cpf varchar2(14) primary key,
+    constraint fk_cpf_executivo foreign key (cpf) references POLITCO(cpf)
+);
+SELECT * FROM EXECUTIVO;
+DROP TABLE EXECUTIVO;
+
+
+-- tabela PARTICIPACOES
+create table PARTICIPACOES(
+    cpf varchar2(14),
+    participacao varchar2(50),
+    CONSTRAINT pk_participacoes primary key (cpf, participacoes),
+    constraint fk_cpf_legislativo foreign key (cpf) references LEGISLATIVO (cpf)
+);
+SELECT * FROM PARTICIPACOES;
+DROP TABLE PARTICIPACOES;
+
+
+-- tabela CRIA
+create table CRIA(
+    cpf varchar2(14),
+    numero integer,
+    constraint pk_cria primary key (cpf, numero),
+    constraint fk_cpf_cria foreign key (cpf) references POLITICO(cpf),
+    constraint fk_numero_cria foreign key (numero) references PROJETO_DE_LEI(numero)
+);
+SELECT * FROM CRIA;
+DROP TABLE CRIA;
 
 
 -- tabela PROJETO_DE_LEI
 create table PROJETO_DE_LEI (
-    autor varchar2(100), -- tem que ser um dos politicos
     ano varchar2(4),
     numero integer primary key,
-    constraint fk_projeto_autor foreign key (autor) references LEGISLATIVO (nome)
+    status varchar2(50)
 );
 SELECT * FROM PROJETO_DE_LEI;
 DROP TABLE PROJETO_DE_LEI;
--- tabela PROJETO_DE_LEI
+
+
+-- tabela ANALISA
+create table ANALISA(
+	numero integer,
+	cpf_exec varchar2(14),
+	data_analisa date,
+	resultado varchar2(50),
+	constraint pk_analisa primary key (numero, cpf_exec),
+	constraint fk_numero_analisa foreign key (numero)references PROJETO_DE_LEI(numero),
+	constraint fk_cpfexec_analisa foreign key (cpf_exec)references EXECUTIVO(cpf)
+);
+SELECT * FROM ANALISA;
+DROP TABLE ANALISA;
 
 
 -- tabela MINISTROS_STF
@@ -29,7 +118,6 @@ create table MINISTROS_STF(
 );
 SELECT * FROM MINISTROS_STF;
 DROP TABLE MINISTROS_STF;
--- tabela MINISTROS_STF
 
 
 -- tabela TITULOS_STF
@@ -38,164 +126,70 @@ create table TITULOS_STF(
     data_titulo date,
     descricao varchar2(300),
     grau varchar2(50),
+    constraint pk_titulos_stf primary key (cpf, data_titulo, descricao, grau),
     constraint fk_cpf_ministro foreign key (cpf) references MINISTROS_STF (cpf)
 );
 SELECT * FROM TITULOS_STF;
 DROP TABLE TITULOS_STF;
--- tabela TITULOS_STF
 
 
 -- tabela LEI
 create table LEI(
     numero integer primary key,
-    autor varchar2(100),
     data_publicacao date not null,
-    constraint fk_lei_autor foreign key (autor) references PROJETO_DE_LEI (autor)
+    numero_pl integer unique not null,
+    cpf_exec varchar2(14) unique not null,
+    constraint fk_numero_pl foreign key (numero_pl, cpf_exec) references ANALISA (numero, cpf_exec)
 );
 SELECT * FROM LEI;
 DROP TABLE LEI;
 -- tabela LEI
 
 
--- tabela MINISTROS
-create table MINISTROS(
-    cpf varchar2(14) primary key,
-    data_nascimento date not null
+-- tabela AUTOR_LEIS
+create table AUTOR_LEIS(
+    numero integer,
+    autor varchar2(100),
+    constraint pk_autor_leis primary key (numero, autor),
+    constraint fk_numero_lei foreign key (numero) references LEI(numero)
+    -- constraint chk_autor check (autor in POLITICO(nome))
 );
-SELECT * FROM MINISTROS;
-DROP TABLE MINISTROS;
--- tabela MINISTROS
 
 
--- tabela MINISTERIOS
-create table MINISTERIOS(
-    sigla varchar2(10) primary key,
-    verba float,
-    nome VARCHAR2(100) not null
+-- tabela MINISTERIOS_MINISTROS
+create table MINISTERIOS_MINISTROS(
+    cpf varchar(14) primary key,
+    data_nascimento date,
+    sigla varchar(10) unique not null,
+    nome_ministerio varchar(50),
+    verba decimal(9,2),
+    cpf_exec varchar(14),
+    constraint fk_cpfexec_ministeriosministros foreign key (cpf_exec) references EXECUTIVO(cpf) 
 );
-SELECT * FROM MINISTERIOS;
-DROP TABLE MINISTERIOS;
--- tabela MINISTERIOS
+SELECT * FROM MINISTERIOS_MINISTROS;
+DROP TABLE MINISTERIOS_MINISTROS;
+
 
 
 -- tabela SECRETARIAS
 create table SECRETARIAS(
-    sigla_min varchar2(10),
+    cpf_ministro varchar2(12),
     sigla varchar2(10),
     nome varchar2(50),
-    constraint fk_sigla_sec foreign key (sigla_min) references MINISTERIOS (sigla)
+    constraint pk_secretaria primary key (cpf_ministro, sigla),
+    constraint fk_cpf_sec foreign key (cpf_ministro) references MINISTERIOS_MINISTROS (cpf)
 );
 SELECT * FROM SECRETARIAS;
 DROP TABLE SECRETARIAS;
--- tabela SECRETARIAS
 
 
--- tabela LEGISLATIVO
-create table LEGISLATIVO (
-    cpf varchar2(14) primary key,
-    cargo varchar2(50) not null,
-    estado varchar2(2) not null,
-    nome varchar2(100) not null,
-    partido varchar2(10)
+-- tabela TRAMITA
+create table TRAMITA(
+    numero integer,
+    tipo_camara varchar2(50),
+    data_tramita date,
+    resultado varchar2(50),
+    constraint pk_tramita primary key (numero, tipo_camara),
+    constraint fk_numero foreign key (numero) references PROJETO_DE_LEI (numero),
+    constraint fk_tipo_camara foreign key (tipo_camara) references CAMARA (tipo)
 );
-
-SELECT * FROM LEGISLATIVO;
-
-DROP TABLE LEGISLATIVO;
--- tabela LEGISLATIVO
-
-
--- tabela EXECUTIVO
-create table EXECUTIVO (
-    cpf varchar2(14) primary key,
-    cargo varchar2(50) not null,
-    estado varchar2(2) not null,
-    nome varchar2(100) not null,
-    partido varchar2(10)
-);
-
-SELECT * FROM EXECUTIVO;
-
-DROP TABLE EXECUTIVO;
--- tabela EXECUTIVO
-
-
--- tabela PARTICIPACOES
-create table PARTICIPACOES(
-    cpf varchar2(14),
-    participacao varchar2(50),
-    constraint fk_cpf_legislativo foreign key (cpf) references LEGISLATIVO (cpf)
-);
--- tabela PARTICIPACOES
-
-
-
-
--- Populando as tabelas
-
--- Inserindo eleitores
-insert into ELEITOR (nome, endereco, tit_eleitor) values
-('Maria Madalena da Silva Santos', 'Rua do Cabo, 183, Ponta Grossa', '111111111111');
-
-insert into ELEITOR (nome, endereco, tit_eleitor) values
-('Joaquim Raimundo Farias', 'Rua dos Bobos, 634, Casa Amarela', '222222222222');
-
-insert into ELEITOR (nome, endereco, tit_eleitor) values
-('Thiago Lorenzo da Silva Ferraz', 'Rua Amauri Fernandes, 456, Várzea', '333333333333');
-
-insert into ELEITOR (nome, endereco, tit_eleitor) values
-('Severino Daniel Emanuel Rocha', 'Rua Ayrton Senna, 567, São Gonçalo', '444444444444');
-
-insert into ELEITOR (nome, endereco, tit_eleitor) values
-('Sebastiana Rayssa Farias', 'Rua Paulo Freire, 75, Várzea', '555555555555');
-
-insert into ELEITOR (nome, endereco, tit_eleitor) values
-('Isis Alícia Rodrigues', 'Rua Demétrio Costa, 10, Pitombeiras', '666666666666');
-
-insert into ELEITOR (nome, endereco, tit_eleitor) values
-('Ana Bezerra da Silva', 'Rua do Paz, 45, Batateira', '777777777777');
--- Inserindo eleitores
-
--- Inserindo projetos
-insert into PROJETO_DE_LEI (autor, ano, numero) values
-('Joana Castro Bezerra', '2010', 784);
-
-insert into PROJETO_DE_LEI (autor, ano, numero) values
-('Cícera Giomar Bezerra', '2018', 645);
-
-insert into PROJETO_DE_LEI (autor, ano, numero) values
-('Joana Castro Bezerra', '2014', 342);
-
-insert into PROJETO_DE_LEI (autor, ano, numero) values
-('Neymar da Silva Santos Júnior', '2022', 754);
-
-insert into PROJETO_DE_LEI (autor, ano, numero) values
-('José Roberto de Sá', '2015', 546);
--- Inserindo projetos
-
--- Inserindo LEGISLATIVO
-insert into LEGISLATIVO (cpf, cargo, estado, nome, partido) values
-('111.111.111-11', 'deputado federal', 'SP', 'José Malaquias Peixoto', 'PSDB');
-
-insert into LEGISLATIVO (cpf, cargo, estado, nome, partido) values
-('222.222.222-22', 'deputado estadual', 'PB', 'Joana Castro Bezerra', 'PT');
-
-insert into LEGISLATIVO (cpf, cargo, estado, nome, partido) values
-('333.333.333-33', 'senador', 'PE', 'José Roberto de Sá', 'PT');
-
-insert into LEGISLATIVO (cpf, cargo, estado, nome, partido) values
-('444.444.444-44', 'deputado federal', 'SP', 'Neymar da Silva Santos Júnior', 'PSOL');
-
-insert into LEGISLATIVO (cpf, cargo, estado, nome, partido) values
-('555.555.555-55', 'deputado federal', 'SP', 'Cícera Giomar Bezerra', 'PSDB');
-
-insert into LEGISLATIVO (cpf, cargo, estado, nome, partido) values
-('666.666.666-66', 'deputado estadual', 'PE', 'Antônio Nunes Ferreira', 'PSL');
--- Inserindo LEGISLATIVO
-
--- Inserindo EXECUTIVO
-insert into EXECUTIVO (cpf, cargo, estado, nome, partido) values
-('777.777.777-13', 'presidente', 'DF', 'Luiz Inácio Lula da Silva', 'PT');
--- Inserindo EXECUTIVO
-
--- Populando as tabelas
